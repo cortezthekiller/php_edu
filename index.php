d9443ab9bb5390c06d5ab66453360a2d5443583c
Rod Gonzalez <rodrigo.decroly@gmail.com>
2012-03-23 23:42:42 +0100

    added script.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?
+// html tags
 echo "<html>";
 echo "<head>";
 echo "<title>PHP edu</title>";
 echo "</head>";
 echo "<body>";
+
+// In the body section. We can now echo something
+echo "I'm using github<br/>";
 echo "</body>";
 echo "</html>";
 ?>
