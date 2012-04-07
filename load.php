5bb2b8f3db3f051f2fd910ccdf651de1075ef346
Rod <rodrigo.decroly@gmail.com>
2012-04-07 13:13:36 +0200

    Changes in 'load.php': added code to automatically insert registers in secondary tables.

--- load.php
+++ load.php
@@ -1,8 +1,10 @@
 <?
 include("/var/seguridad/db.inc.php");
 
-$file  = "datos.txt";   /* Archivo de texto con los datos de los alumnos */
-$table = "alumnos";
+$file   = "datos.txt";   /* Archivo de texto con los datos de los alumnos */
+$table  = "alumnos";     /* Tabla principal */
+$tables = array("matematicas","historia","tecnologia"); /* Tablas vinculadas */
+
 
 /* Conectamos con el servidor y comprobamos la conexión */
 $link = mysql_connect($mysql_host, $mysql_user, $mysql_passwd)
@@ -15,12 +17,25 @@ $query .= " REPLACE INTO TABLE ".$table;
 $query .= " FIELDS TERMINATED BY ','";
 $query .= " LINES TERMINATED BY '\n' ";
 
-echo $query;
-mysql_query($query, $link) or die("Error LOAD DATA: ".$mysql_error());
+mysql_query($query, $link) or die("Error LOAD DATA: ".$mysql_error($link));
 
 /* Una vez cargados los datos en la tabla principal, procedemos a */
 /* insertar los correspondientes registros en las tablas de las   */
 /* asignaturas (vinculadas por medio de DNI como clave foránea).  */
 
+$query  = "SELECT DNI FROM ".$table;
+$result = mysql_query($query, $link);
+
+while($fila = mysql_fetch_array($result, MYSQL_ASSOC)) {
+
+   foreach($tables as $tablename) {
+      $insert = "INSERT INTO ".$tablename." (DNI) VALUES ('".$fila['DNI']."')";
+      mysql_query($insert, $link) 
+         or die("Error en INSERT: ".mysql_error($link));
+   }
+}
+
+/* Liberar recursos y cerrar conexión con servidor */
+mysql_free_result($result);
 mysql_close($link);
 ?>
